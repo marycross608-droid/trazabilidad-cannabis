50a8a50de83a2339e62730e8e16be6eac732ec0b
Maria Cruz <marycross608@gmail.com>
2025-12-07 01:46:11 -0500

    Config OpenAPI

---
+++ src/main/java/com/cruz/trazabilidadcannabis/config/OpenAPIConfig.java
@@ -0,0 +1,31 @@
+package com.cruz.trazabilidadcannabis.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API de Trazabilidad Cannabis")
+                        .version("1.0.0")
+                        .description("Sistema para gestionar etapas de cultivo y trazabilidad en el sector agroindustrial")
+                        .contact(new Contact()
+                                .name("Mary Soto")
+                                .email("mary.soto@empresa.com")
+                                .url("https://www.empresa.com")
+                        )
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")
+                        )
+                );
+    }
+}
